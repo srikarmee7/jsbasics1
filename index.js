ccc477695985b39eb14594c4818f4c6260d40583
srikarmee7 <srikarmeesaragandla001@gmail.com>
2022-10-09 17:54:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("hi")
+let a = 10
+let b = "sfd"
+let c = 's'
+let d = 10.3
+let e = true
+let f = 
+
+console.log(typeof a)
+console.log(typeof b + typeof c)
+
+// let user = window.prompt("What's your name?")
+
+// window.alert("Hey! there " + user)
+
